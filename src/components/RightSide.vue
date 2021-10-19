8f57b1158f8e0efb7e012d05bad0d72e53d1c477
Arc_Anya <arcanyatwitch@gmail.com>
2021-10-19 22:04:36 +0200

    adding logic to detect Keplr Wallet

--- src/components/RightSide.vue
+++ src/components/RightSide.vue
@@ -3,7 +3,7 @@
     <div class="header">
       <div>SEARCH</div>
       <div>THEME</div>
-      <button>Connect Wallet #1</button>
+      <button @click="getKeplr()">Connect Wallet #1</button>
     </div>
     <br />
     <div class="list">List of Chains (Components) #2</div>
@@ -15,6 +15,36 @@ export default {
   name: "RightSide",
   props: {
     // msg: String
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    async getKeplr() {
+      if (window.keplr) {
+        return window.keplr;
+      } else {
+          console.log("No Kelpr!")
+      }
+
+      if (document.readyState === "complete") {
+        return window.keplr;
+      }
+
+      return new Promise((resolve) => {
+        const documentStateChange = (event) => {
+          if (event.target && event.target.readyState === "complete") {
+            resolve(window.keplr);
+            document.removeEventListener(
+              "readystatechange",
+              documentStateChange
+            );
+          }
+        };
+
+        document.addEventListener("readystatechange", documentStateChange);
+      });
+    },
   },
 };
 </script>
